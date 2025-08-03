37deba59ec7e50228fc7864d9cb10ec9e1fc860c
akshata <akshataskawale@outlook.com>
2025-08-03 17:19:57 +0530

    api added in url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mini-linkedinclone.onrender.com",
+  baseURL: "https://mini-linkedinclone.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -20,7 +20,7 @@ function Register() {
     try {
       const res = await API.post("/auth/register", form);
       localStorage.setItem("token", res.data.token);
-      //navigate("/");
+      navigate("/");
     } catch (err) {
       alert("Registration failed");
     }
