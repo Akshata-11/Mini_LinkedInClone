710727459cf97f07f526c6c48e7c1aabd45eecc4
akshata <akshataskawale@outlook.com>
2025-08-03 14:35:57 +0530

    frontend:base uri added in api.js-2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mini-linked-in-clone.vercel.app/",
+  baseURL: "https://mini-linked-in-clone.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
