82296831dca5f7cb2ed7b25d19704218d222a273
akshata <akshataskawale@outlook.com>
2025-08-03 15:54:53 +0530

    api updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const userRoutes = require("./routes/user");
 const app = express();
 // app.use(cors({ origin: "https://mini-ciaan-linke.netlify.app/login" }));
 
-const allowedOrigins = ["https://mini-ciaan-linke.netlify.app/api"];
+const allowedOrigins = ["https://mini-ciaan-linke.netlify.app"];
 
 app.use(
   cors({
